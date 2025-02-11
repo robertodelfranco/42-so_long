9576506ddcf28c1bed27d7152ca6210764b0aa3a
Roberto Del Franco <robertinhodf@gmail.com>
2025-02-11 14:11:29 -0300

    fixing norm and placing trees

--- src/handle_decorations.c
+++ src/handle_decorations.c
@@ -65,3 +65,50 @@ void	find_tile(t_game *game)
 		flag = i % 2;
 	}
 }
+
+void	mushroom_rand(t_game *game, int i, int flag)
+{
+	static int	rand = 0;
+
+	if (game->map->map[i][game->map->width - 2] == '1' && flag == 0)
+		mlx_image_to_window(game->mlx, game->img->mushroom_img,
+			(game->map->width - 2) * TILE, i * TILE);
+	else if (game->map->map[i][game->map->width - 3] == '1' && flag == 1)
+		mlx_image_to_window(game->mlx, game->img->mushroom_img,
+			(game->map->width - 3) * TILE, i * TILE);
+	else
+	{
+		rand += 1;
+		mlx_image_to_window(game->mlx, game->img->mushroom_img,
+			(game->map->width - rand) * TILE, i * TILE);
+	}
+}
+
+void	put_mushroons(t_game *game, mlx_image_t *mushroom_img)
+{
+	int	i;
+	int	j;
+	int	rand;
+
+	i = -1;
+	j = (game->map->width - 4);
+	rand = game->map->width / 2;
+	while (i++, game->map->map[i])
+	{
+		j = i % 2;
+		if (game->map->map[i][rand] == '1' && i != game->map->height - 1)
+		{
+			mlx_image_to_window(game->mlx, mushroom_img, rand * 64, i * 64);
+			if (game->map->map[i + 1][rand + 2] == '1')
+				rand += 2;
+			else if (game->map->map[i + 1][rand + 1] == '1')
+				rand += 1;
+			else
+				rand -= 3;
+		}
+		if (j == 1 && game->map->map[i][0] == '1')
+			mlx_image_to_window(game->mlx, mushroom_img, 0 * 64, i * 64);
+		else
+			mushroom_rand(game, i, i / 3);
+	}
+}
